8c7628ab9147a9eda2cfa9976063968b375cd7c1
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-01-29 18:50:58 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// 1
+let result = 5+5+"5"
+console.log(result)
+
+// 2
+let email = "exaple69@gmail.com"
+console.log("It's", email.includes("@") ,"that your email does include @")
+console.log("Your email is", email.length, "characters long")
+
+// 3
+let first = "My "
+let second = "name"
+let third = " is "
+let fullName = first + second + third + "Victor"
+console.log(fullName)
+// 4
+let userName = prompt("What's your name?")
+let payment = ("1,000,000$")
+alert(`Дякуємо, ${userName}, До сплати ${payment} :), інакше...`)
